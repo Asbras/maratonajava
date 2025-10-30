db5495dae9359f9eb026697152b1b58b90c67c40
Asbras <silentbob320@gmail.com>
2025-10-30 12:06:30 -0300

    feat: Adicionar classe que imprime um vetor com seus valores multiplicados por 2

--- src/maratonajava/Exercicios/test/PrimoTeste.java
+++ src/maratonajava/Exercicios/test/PrimoTeste.java
@@ -3,6 +3,7 @@ package maratonajava.Exercicios.test;
 import maratonajava.Exercicios.domain.Primo;
 
 public class PrimoTeste {
+//      Criar um metodo que checa se um número é primo ou não. Caso seja primo retorne true, caso não retorne false;
     public static void main(String[] args) {
         Primo primo = new Primo();
 
@@ -11,7 +12,9 @@ public class PrimoTeste {
         System.out.println("3 - " + primo.isPrime(3));  // true
         System.out.println("4 - " +primo.isPrime(4));  // false (pelo %2)
         System.out.println("9 - " +primo.isPrime(9));  // false (encontra i=3)
-        System.out.println("17 - " +primo.isPrime(17));
-        System.out.println("1703 - " +primo.isPrime(1703));
+        System.out.println("17 - " +primo.isPrime(17)); // true
+        System.out.println("1703 - " +primo.isPrime(7919)); // true
+        System.out.println("1703 - " +primo.isPrime(7920)); // false
+
     }
 }
