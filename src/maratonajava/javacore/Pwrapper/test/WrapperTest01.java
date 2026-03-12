71ddc898f5321014381e92ac5824d3f4386affac
Asbras <silentbob320@gmail.com>
2026-03-12 17:08:06 -0300

    feat: 106 - Utility Classes - Wrappers pt 01

---
+++ src/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,27 @@
+package maratonajava.javacore.Pwrapper.test;
+
+import java.util.List;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 100;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        Byte byteW = 127;
+        Short shoWtW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 100.0;
+        Character chaWW = 'W';
+        Boolean booleanW = false;
+
+        List<Integer> list;
+    }
+}
